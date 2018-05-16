53f63ee84e20e0ed0c474d271c828f11932ab40e
Capt-Herge <capt_herge@freenet.de>
2018-05-16 14:42:55 +0200

    Class-Header hinzugefügt

--- Client/Client/Hergen.cs
+++ Client/Client/Hergen.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* Author: Hergen Paradies
+ * Matr.Nr.: 70452163
+ * Fach: Threadprogrammierung(?)
+ * Aufgabe:
+ * 
+ * Anmerkung(en):
+ */
 namespace Client
 {
+    
     class Hergen
     {
+
     }
 }
