bb5a247b1596ada51e8cdd04e5e9b59af315998d
Capt-Herge <capt_herge@freenet.de>
2018-05-19 11:38:24 +0200

    Premerge commit

--- Client/Client/Hergen.cs
+++ Client/Client/Hergen.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 /* Author: Hergen Paradies
  * Matr.Nr.: 70452163
@@ -15,6 +17,21 @@ namespace Client
     
     class Hergen
     {
+        #region fields
+        DataTable _table;
+        Daniel _daniel;
+        #endregion
 
+        #region ctor
+        public Hergen(DataTable table, Daniel daniel)
+        {
+            _table = table;
+            _daniel = daniel;
+        }
+        #endregion
+
+        #region methods
+
+        #endregion
     }
 }
