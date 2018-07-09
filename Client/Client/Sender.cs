cfd2cc40bd386d54fdcb20d5cc511af03b45526d
Riccel <daniel.wolff2@web.de>
2018-07-09 14:54:42 +0200

    Sender.cs nachgereicht

---
+++ Client/Client/Sender.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Sender
+    {
+        #region fields
+        private List<byte[]> _list;
+        private System.Threading.Semaphore S;
+        private IPAddress _ipaddress;
+        private int _port;
+        #endregion
+
+        #region ctor
+        public Sender(List<byte[]> list, int parallelThreads, IPAddress ipaddress, int port)
+        {
+            _list = list;
+            S = new System.Threading.Semaphore(parallelThreads, parallelThreads);
+            _ipaddress = ipaddress;
+            _port = port;
+        }
+
+        #endregion
+
+        #region methods
+        private void Run()
+        {
+            ManualResetEvent resetEvent = new ManualResetEvent(false);
+            int toProcess = _list.Count() - 1;
+
+            for (int i = 0; i < _list.Count() - 2; i++)
+            {
+                new Thread(delegate ()
+                {
+                    //Spawnen des Workers
+                    Worker(_list[i]);
+                    // Wenn dies der letzte Thread ist, Signal absetzen
+                    if (Interlocked.Decrement(ref toProcess) == 0)
+                    {
+                        resetEvent.Set();
+                    }
+                }).Start();
+            }
+
+            //Warten bis alle Threads fertig sind
+            resetEvent.WaitOne();
+            Console.WriteLine("Alle Threads fertig.");
+
+            // RootDoc abarbeiten (Letzter Eintrag in _list)
+            Worker(_list[_list.Count() - 1]);
+        }
+
+        private void Worker(byte[] bear)
+        {
+            bool sent = false;
+            
+            while (!sent)
+            {
+                try
+                {
+                    Socket mysocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    mysocket.Connect(_ipaddress, _port);
+                    mysocket.Send(bear);
+                    mysocket.Close();
+                    Console.WriteLine("Ein Bär wurde gesendet.");
+                    sent = true;
+                }
+                catch
+                {
+                    Thread.Sleep(30);
+                }
+            }
+
+        }
+
+    
+
+        #endregion
+
+    }
+}
